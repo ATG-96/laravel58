924de52001522133899fdb4f05aa57446f3a08de
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-20 21:50:57 +0900

    chart button

--- resources/views/admin/dashboardchart.blade.php
+++ resources/views/admin/dashboardchart.blade.php
@@ -59,6 +59,32 @@
    </div>
    
   </div>
+
+  <div class="card-body">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>user_id</th>
+              <th>user_name</th>
+              <th>line_id</th>
+              <th>temp</th> 
+              <th>temp_time</th> 
+            </thead>
+            <tbody>                
+              @foreach ($tempcheck as $rowtempcheck)
+                <tr>
+                <td>{{ $rowtempcheck->user_id }}</td>
+                <td>{{ $rowtempcheck->user_name }}</td>
+                <td>{{ $rowtempcheck->line_id }}</td>
+                <td>{{ $rowtempcheck->temp }}</td>
+                <td>{{ $rowtempcheck->temp_time }}</td>                    
+                </tr>
+              @endforeach                   
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
   
  </body>
 </html>
