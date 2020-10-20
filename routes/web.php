749e754e1da207f63d1d58b96997d7cad42c4472
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-20 19:53:27 +0900

    chart first try2

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::group(['middleware' => ['auth', 'admin']], function () {
     Route::get('/dashboard', 'Admin\DashboardController@getSafeCheck');
     Route::get('/dashboard1','Admin\DashboardController@dashboard1');
     Route::get('/dashboard2','Admin\DashboardController@dashboard2');
+    Route::get('/dashboardchart','LaravelGoogleGraph@dashboardchart');
     Route::get('/role-register','Admin\DashboardController@registered');
 });
 
