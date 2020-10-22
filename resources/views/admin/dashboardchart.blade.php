5dfe0557fd95ec0862155890b708eb01a0f3edfc
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-22 15:24:43 +0900

    add

--- resources/views/admin/dashboardchart.blade.php
+++ resources/views/admin/dashboardchart.blade.php
@@ -61,6 +61,34 @@
    </div>
   </div>
 
+  <div class="col-md-12">
+  <div class="row">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>user_id</th>
+              <th>user_name</th>
+              <th>line_id</th>
+              <th>temp</th> 
+              <th>temp_time</th> 
+            </thead>
+            <tbody>                
+              @foreach ($tempcheck as $rowtempcheck)
+                <tr>
+                <td>{{ $rowtempcheck->user_id }}</td>
+                <td>{{ $rowtempcheck->user_name }}</td>
+                <td>{{ $rowtempcheck->line_id }}</td>
+                <td>{{ $rowtempcheck->temp }}</td>
+                <td>{{ $rowtempcheck->temp_time }}</td>                    
+                </tr>
+              @endforeach                   
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+      </div>
+      
 
   
  </body>
