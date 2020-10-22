93c24f91b4d2fcac20f14905b228467776440fe4
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-22 16:00:46 +0900

    add

--- resources/views/admin/dashboardchart.blade.php
+++ resources/views/admin/dashboardchart.blade.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <style type="text/css">
    .box{
-    width:300px;
+    width:100px;
     margin:0 auto;
    }
   </style>
