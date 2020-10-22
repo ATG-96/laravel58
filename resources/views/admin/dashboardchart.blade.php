b5b68ddebf64cffb43586eec2b8cf13232787344
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-22 16:57:15 +0900

    add

--- resources/views/admin/dashboardchart.blade.php
+++ resources/views/admin/dashboardchart.blade.php
@@ -42,7 +42,7 @@
    }
   </script>
  </head>
- <body leftmargin="100px">
+ <body leftmargin="200px">
   <br />
   <div class="container">
    <h3 align="center">Make Google Pie Chart in Laravel</h3><br />
