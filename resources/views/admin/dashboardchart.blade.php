155502eb3d83ce25ca1da707bdbf10de407e799b
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-26 23:51:55 +0900

    adding bottuns

--- resources/views/admin/dashboardchart.blade.php
+++ resources/views/admin/dashboardchart.blade.php
@@ -26,7 +26,7 @@
 </style> -->
 
   <!-- start container-->
-  <div class="container-fluid" style="margin-top:20px;">
+  <div class="container-fluid" style="margin-top:80px;">
    <!-- <h3 style="text-align:center;margin-top:2%;">Make Google Pie Chart </h3> -->
    
    
