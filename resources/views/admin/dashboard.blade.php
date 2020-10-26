6e7f85a257abd1a78bf793d782298aed7b44e3cb
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-26 12:12:49 +0900

    adding bottuns

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -139,6 +139,41 @@
     </div>
   </div>
 
+<div class="row">
+  <div class="col-md-9">
+    <div class="card">
+      <div class="card-header">
+        <h4 class="card-title"> Registered Roles</h4>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table">
+            <thead class=" text-primary">
+              <th>user_id</th>
+              <th>user_name</th>
+              <th>line_id</th>
+              <th>temp</th> 
+              <th>temp_time</th> 
+            </thead>
+            <tbody>                
+              @foreach ($dashtempcheck as $rowtempcheck)
+                <tr>
+                <td>{{ $rowtempcheck->user_id }}</td>
+                <td>{{ $rowtempcheck->user_name }}</td>
+                <td>{{ $rowtempcheck->line_id }}</td>
+                <td>{{ $rowtempcheck->temp }}</td>
+                <td>{{ $rowtempcheck->temp_time }}</td>                    
+                </tr>
+              @endforeach                   
+              
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 @endsection
 
 @section('scripts')
