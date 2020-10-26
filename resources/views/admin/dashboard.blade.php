57972de8083da1843640a7f185a296d82c547660
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-26 11:51:39 +0900

    adding bottuns

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -89,6 +89,56 @@
     </div>
   </div>
 
+  <div class="row">
+    <div class="col-8">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title"> Registered Roles</h4>
+        </div>
+        <div class="card-body">
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>user_id</th>
+                <th>user_name</th>
+                <th>line_id</th>
+                <th>is_safe</th>
+                <th>safe_location</th> 
+                <th>safe_mess</th> 
+                <th>time_update</th>                    
+              </thead>
+              <tbody>                
+                @foreach ($safecheck as $rowsafecheck)
+                  <tr>
+                  <td>{{ $rowsafecheck->user_id }}</td>
+                  <td>{{ $rowsafecheck->user_name }}</td>
+                  <td>{{ $rowsafecheck->line_id }}</td>
+                  <td>{{ $rowsafecheck->is_safe }}</td>
+                  <td>{{ $rowsafecheck->safe_location }}</td>
+                  <td>{{ $rowsafecheck->safe_mess }}</td> 
+                  <td>{{ $rowsafecheck->time_update }}</td>                    
+                  </tr>
+                @endforeach                   
+                
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title"> Detailed Search</h4>
+        </div>
+        <div class="card-body">
+          <button type="button" class="btn btn-success">Safe</button>
+          <button type="button" class="btn btn-danger">Not Safe</button>
+        </div>
+      </div>
+    </div>
+  </div>
+
 @endsection
 
 @section('scripts')
