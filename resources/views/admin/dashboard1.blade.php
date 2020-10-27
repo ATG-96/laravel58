4499e65acdebd87db8ec35ab22f915ae63ed7de0
Buki <AbdullahTAlghanmi@gmail.com>
2020-10-27 18:39:15 +0900

    new names

--- resources/views/admin/dashboard1.blade.php
+++ resources/views/admin/dashboard1.blade.php
@@ -156,44 +156,6 @@
    
   </script>
 
-@endsection
- 
-
-
-    <div class="card">
-      <div class="card-header">
-        <h4 class="card-title"> Registered Roles</h4>
-      </div>
-      <div class="card-body">
-        <div class="table-responsive">
-          <table class="table">
-            <thead class=" text-primary">
-              <th>user_id</th>
-              <th>user_name</th>
-              <th>line_id</th>
-              <th>temp</th> 
-              <th>temp_time</th> 
-            </thead>
-            <tbody>                
-              @foreach ($tempcheck as $rowtempcheck)
-                <tr>
-                <td>{{ $rowtempcheck->user_id }}</td>
-                <td>{{ $rowtempcheck->user_name }}</td>
-                <td>{{ $rowtempcheck->line_id }}</td>
-                <td>{{ $rowtempcheck->temp }}</td>
-                <td>{{ $rowtempcheck->temp_time }}</td>                    
-                </tr>
-              @endforeach                   
-              
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-
-  
 
 @endsection
 
