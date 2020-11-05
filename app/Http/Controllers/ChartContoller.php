d10580108bd3ed2f17099ceb9f4e1edf9dfb12bd
Buki <AbdullahTAlghanmi@gmail.com>
2020-11-05 17:30:44 +0900

    new style of graphs

--- app/Http/Controllers/ChartContoller.php
+++ app/Http/Controllers/ChartContoller.php
@@ -19,17 +19,18 @@ class ChartContoller extends Controller
         //     ->get();
         // // print_r($a);
         
-        $a=DB::table('public.users_line','public.safe_check')
-        ->select(DB::raw('line_userid,displayname,user_id'))
-            ->leftjoin('line_userid','users_line.line_userid', '=', 'safe_check.line_id')
-            ->where('safe_check.line_id', 'IS', NULL)
-            ->get();
+        // $a=DB::table('public.users_line','public.safe_check')
+        // ->select(DB::raw('line_userid,displayname,user_id'))
+        //     ->leftjoin('line_userid','users_line.line_userid', '=', 'safe_check.line_id')
+        //     ->where('safe_check.line_id', 'IS', NULL)
+        //     ->get();
 
 
 
             
-     print_r($a);
+    //  print_r($a);
         $b=2;
+        $a=1;
     $pie  =	 Charts::create('pie', 'highcharts')
         ->title('Rep or not')
         ->labels(['Replied', 'Not repply'])
