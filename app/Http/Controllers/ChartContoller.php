046341f4f7ec2c409a8390c49d2f77cb8ab31f84
Buki <AbdullahTAlghanmi@gmail.com>
2020-11-05 12:41:31 +0900

    new style of graphs

--- app/Http/Controllers/ChartContoller.php
+++ app/Http/Controllers/ChartContoller.php
@@ -8,12 +8,12 @@ use Charts;
 class ChartContoller extends Controller
 {
     public function index(){
-        $pie  =	 Charts::create('pie', 'highcharts')
-      ->title('My nice chart')
-      ->labels(['First', 'Second', 'Third'])
-      ->values([5,10,20])
-      ->dimensions(1000,500)
-      ->responsive(false);
-    return view('admin.chart')->with('pie',$pie);
+    $pie  =	 Charts::create('pie', 'highcharts')
+        ->title('Rep or not')
+        ->labels(['Replied', 'Not repply'])
+        ->values([2,1])
+        ->dimensions(1000,500)
+        ->responsive(false);
+    return view('admin.chart')->with('gender', json_encode($array))->with('pie',$pie);
     }
 }
