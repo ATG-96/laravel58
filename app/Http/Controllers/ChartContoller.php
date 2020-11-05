1e966c2512d83b47d668dead0e673bc475563649
Buki <AbdullahTAlghanmi@gmail.com>
2020-11-05 16:51:45 +0900

    new style of graphs

--- app/Http/Controllers/ChartContoller.php
+++ app/Http/Controllers/ChartContoller.php
@@ -19,7 +19,7 @@ class ChartContoller extends Controller
         //     ->get();
         // // print_r($a);
         
-        $a=DB::table('users_line')
+        $a=DB::table('users_line','safe_check')
         ->select(DB::raw('line_userid,displayname,user_id'))
             ->leftjoin('line_userid','users_line.line_userid', '=', 'safe_check.line_id')
             ->where('safe_check.line_id', 'IS', NULL)
